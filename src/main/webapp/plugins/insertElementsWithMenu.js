a3c0c902f9da1013f74c66aaf852c462edec17b7
mtouil <mehdi.touil@devoteam.com>
2020-02-17 15:43:08 +0100

    VpcId jsonDoc

--- src/main/webapp/plugins/insertElementsWithMenu.js
+++ src/main/webapp/plugins/insertElementsWithMenu.js
@@ -164,7 +164,7 @@ Draw.loadPlugin(function(ui) {
 
         for (let index = 0; index < subnets.length; index++) {
           const subnet = subnets[0];
-          const vpcId = subnet["VpcId"];
+          const vpcId = jsoDoc["Subnets"][index]["VpcID"];
           console.log(vpcId);
         } 
         
